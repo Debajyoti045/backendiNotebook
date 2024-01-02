cd14767a46a0d2d52a7f9121948704c2c0a3f69d
Debajyoti045 <debajyotidas497@gmail.com>
2024-01-02 20:55:18 +0530

    second commit

--- db.js
+++ db.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 const url = process.env.VITE_MONGOURL
 const connectToMongo = async ()=>{
     try {
+        console.log(url);
         await mongoose.connect(url)
         console.log("mongoDb connected successfully!")
     } catch (error) {
